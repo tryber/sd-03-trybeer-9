a7510c01db0992e2b6329da90186e99ea7916180
Marco Barbosa <marco.meireles.b@gmail.com>
2020-09-24 16:16:52 -0300

    rota atualizada

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,12 +10,14 @@ import Profile from './components/Profile';
 import Home from './components/Home';
 import Products from './components/Products';
 import UserRegisterPage from './pages/User-Register';
+import Checkout from './pages/CheckoutPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/admin/orders" />
+        <Rot path="/checkout" component={ Checkout } />
         <Route path="/login" component={ Login } />
         <Route path="/profile" component={ Profile } />
         <Route path="/products" component={ Products } />
