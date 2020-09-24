8eac0dc615788f23c2fdae7271282de76ff9bbba
Marco Barbosa <marco.meireles.b@gmail.com>
2020-09-24 16:10:50 -0300

    Checkout Hello World!

---
+++ front-end/src/pages/CheckoutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuTop from '../components/MenuTop';
+import Checkout from '../components/Checkout.jsx';
+
+function CheckoutPage() {
+  return (
+    <div>
+      <MenuTop />
+      <Checkout />
+    </div>
+  );
+}
+
+export default CheckoutPage;
