6c96953c3a30085732a5c3cd167823b3137d9443
William Igor <williamigorsilva@gmail.com>
2020-09-28 20:25:05 -0300

    admin orders

--- front-end/src/components/FakeAdmin.jsx
+++ front-end/src/components/FakeAdmin.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { BeerContext } from '../context/context';
 
 // requisito 9 ? MenuTopAdmin : FakeAdmin
 
+const cardOrders = (numOrder, adress, total, status, i) => (
+  <Link to="/admin/orders/:id">
+    <div>
+      <p data-testid={`${i}-order-number`}>Pedido: {numOrder}</p>
+      <p data-testid={`${i}-order-address`}>Endereço: {adress}</p>
+      <p data-testid={`${i}-order-total-value`}>R$ {total}</p>
+      <label data-testid={`${i}-order-status`}>{status}</label>
+    </div>
+  </Link>
+);
+
 const FakeAdmin = () => {
+  const { cart } = useContext(BeerContext);
+
   return (
     <div>
-      ola admin
       <Link data-testid="side-menu-item-profile" to="/admin/profile" >Profile</Link>
+      {cart?.map((e, i) => cardOrders(e.pedido, e.endereco, e.total, e.status, i))}
     </div>
   );
 }
