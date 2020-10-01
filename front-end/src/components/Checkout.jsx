d6bd3b15138d66af6dc154b7448cd79a6ecc30ff
Marco Barbosa <marco.meireles.b@gmail.com>
2020-10-01 10:41:30 -0300

    Pontape inicial

--- front-end/src/components/Checkout.jsx
+++ front-end/src/components/Checkout.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import SaleOrderAPI from '../services/SaleOrderService';
 
+// Commit inicial
+
 const createOrderAPI = async ({
       userId,
       totalPrice,
