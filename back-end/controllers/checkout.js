3923bfbc171338861f90a8119ec7886534626b1d
Marco Barbosa <marco.meireles.b@gmail.com>
2020-10-06 20:20:01 -0300

    frontend trybeer para o heroku

--- back-end/controllers/checkout.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const checkoutService = require('../services/checkoutService');
-
-const checkout = Router();
-
-checkout.post('/', checkoutService);
-
-module.exports = checkout;
